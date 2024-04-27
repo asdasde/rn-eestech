88b25556c9b1379961e30f60a1e05a4aa8b11829
Vanja <vujovic.vanjanja@gmail.com>
2024-04-27 18:16:45 +0200

    Added functionality.js and tests2.json

--- functionality.js
+++ functionality.js
@@ -4,5 +4,22 @@ const form = document.createElement('form');
 const input = document.createElement('input');
 input.type = 'file';
 input.accept = 'application/json';
+
+const files = [];
+
+const button = document.createElement('button');
+button.style.padding = '8px 16px';
+button.style.margin = '8px';
+
 form.appendChild(input);
+form.appendChild(button);
 document.body.appendChild(form);
+
+input.addEventListener('change', () => {
+    files.push(input.files[0]);
+});
+
+button.addEventListener('click', () => {
+    event.preventDefault();
+    
+});
